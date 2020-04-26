be678addef8eaa0f8c76dd19efdab87518f7e0e0
Zubair Bashir <zubair.bashirzubair@gmail.com>
2020-04-27 02:11:24 +0530

    Ignore the build directory

--- build/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Sorter_1 = require("./components/Sorter");
-var NumericCollection_1 = require("./components/NumericCollection");
-var collection = new NumericCollection_1.NumericCollection([10, 9, -2, 1]);
-var sorter = new Sorter_1.Sorter(collection);
-sorter.sort();
-console.log(collection.data);
