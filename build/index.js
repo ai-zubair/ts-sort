7a6ce20f84b60d6d95b37236be0350a650306352
Zubair Bashir <zubair.bashirzubair@gmail.com>
2020-04-26 23:50:32 +0530

    Set up the sorter module

--- build/index.js
+++ build/index.js
@@ -1,2 +1,22 @@
 "use strict";
-console.log("Hello world this is me and i am loving it");
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length; //array is essentially an object with a length property
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - 1 - i; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    var temp = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = temp;
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
+var sorter = new Sorter([10, 0, -1, 5, -2, 1, 23, 4]);
+sorter.sort();
+console.log(sorter.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello world this is me and i am loving it")
+class Sorter{
+
+  constructor(public collection: number[]){}
+
+  sort(): void{ //uses bubble sort
+    const { length } = this.collection; //array is essentially an object with a length property
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - 1 - i; j++) {
+        if(this.collection[j] > this.collection[j+1]){
+          const temp = this.collection[j];
+          this.collection[j] = this.collection[j+1];
+          this.collection[j+1] = temp;
+        }
+      }
+      
+    }
+  }
+
+}
+
+const sorter = new Sorter([10,0,-1,5,-2,1,23,4]);
+sorter. sort();
+console.log(sorter.collection);
