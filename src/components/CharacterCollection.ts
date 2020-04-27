f5cd14c17a6cde4f957de99e33d555a573914b6e
Zubair Bashir <zubair.bashirzubair@gmail.com>
2020-04-27 14:23:01 +0530

    Add character collection module

--- src/components/CharacterCollection.ts
+++ src/components/CharacterCollection.ts
@@ -8,12 +8,20 @@ class CharacterCollection{
   }
 
   compare(leftIndex: number, rightIndex: number): boolean{ //compares 2 numbers and returns a boolean indicating whether the numbers at the specified indices should be swapped
-    return this.data[leftIndex] > this.data[rightIndex];
+    return this.data[leftIndex].toUpperCase() > this.data[rightIndex].toUpperCase();
   }
 
   swap(leftIndex: number, rightIndex: number): void{ //swaps the numbers at the specified indices
-    
-  }
+    const characters = this.data.split('');
+
+    /* we can now user array bsed swapping */
+    const leftValue = characters[leftIndex];
+    characters[leftIndex] = characters[rightIndex];
+    characters[rightIndex] = leftValue;
+
+    /* re-assign the modified string */
+    this.data = characters.join('');
+  } 
 }
 
 export { CharacterCollection }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import { Sorter } from "./components/Sorter";
 import { NumericCollection } from "./components/NumericCollection";
+import { CharacterCollection } from "./components/CharacterCollection";
 
-const collection = new NumericCollection([2,-1,44,1]);
-const sorter = new Sorter(collection);
-sorter.sort();
+const numCollection = new NumericCollection([2,-1,44,1]);
+const charCollection = new CharacterCollection("XarQCcpzB");
 
-console.log(collection.data);
+const numSorter = new Sorter(numCollection);
+const charSorter = new Sorter(charCollection);
+
+numSorter.sort();
+charSorter.sort();
+
+console.log(numCollection.data);
+console.log(charCollection.data);
